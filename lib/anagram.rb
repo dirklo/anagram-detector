46894abce56f1548d236737a496898b8f21313d3
Rick <dirklo@gmail.com>
2020-12-09 22:18:37 +0100

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Anagram
+    def initialize(first_word)
+        @first_word = first_word
+    end
+
+    def match(some_words)
+        final = []
+        some_words.each do |word|
+            if word.split("").sort == @first_word.split("").sort
+                final << word
+            end
+        end
+        final
+    end
+end
